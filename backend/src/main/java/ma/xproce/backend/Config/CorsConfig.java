749f11979f5c6e8e376836128df798a706bae2a4
doha <dohaezzegrani@gmail.com>
2024-11-17 18:10:14 +0100

    v3

--- backend/src/main/java/ma/xproce/backend/Config/CorsConfig.java
+++ backend/src/main/java/ma/xproce/backend/Config/CorsConfig.java
@@ -1,2 +1,23 @@
-package ma.xproce.backend.Config;public class CorsConfig {
+package ma.xproce.backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000", "http://10.0.2.2:3000") // Add all relevant origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
--- backend/src/main/java/ma/xproce/backend/web/UserController.java
+++ backend/src/main/java/ma/xproce/backend/web/UserController.java
@@ -1,8 +1,11 @@
 package ma.xproce.backend.web;
 
 import ma.xproce.backend.Dao.entities.User;
+import ma.xproce.backend.Dao.repositories.UserRepository;
 import ma.xproce.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,12 +14,21 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     // Créer un nouvel utilisateur
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        System.out.println("Received registration request: " + user);
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cet email est déjà utilisé");
+        }
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.OK).body("Inscription réussie");
     }
+
+
 
     // Obtenir un utilisateur par son ID
     @GetMapping("/{id}")
